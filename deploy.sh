8e236379869ae0b811ad21544044c023b99f9a3b
changyi <changyi@ayia.local>
2020-06-10 15:09:30 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git push -f $githubUrl master:gh-pages # 推送到github
 # deploy to coding
 # echo 'www.zerodegree.top\nzerodegree.top' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-   codingUrl=git@e.coding.net:CYi1113/CYi1113/Blog.git
+   codingUrl=git@github.com:CYi1113/Blog.git
 # else
 #    codingUrl=https://VzpWUthwxq:${CODING_TOKEN}@e.coding.net/zerodegree/zerodegree.git #注意！！！这里需要使用coding提供的个人令牌的用户名和token
  fi
